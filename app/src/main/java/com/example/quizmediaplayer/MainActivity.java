61ac2107da465a468d8a396a23c0c06078fccdfb
NM-NM5 <NM-NM5@github.com>
2020-04-05 12:05:15 +0200

    the whole app

---
+++ app/src/main/java/com/example/quizmediaplayer/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.quizmediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.mostafa);
+
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "play", Toast.LENGTH_LONG).show();
+                mediaPlayer.start();
+            }
+        });
+
+        Button pauseButton = findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "pause", Toast.LENGTH_LONG).show();
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
